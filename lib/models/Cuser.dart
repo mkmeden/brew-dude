265561871dd145c9c7b3669e38191587a5ae8bf5
mkmeden <midhupayathuparambil@gmail.com>
2024-04-02 23:04:22 +0530

    fully recovered

---
+++ lib/models/Cuser.dart
@@ -0,0 +1,5 @@
+class CUser{
+
+  String? uid;
+  CUser({required this.uid});
+}
--- lib/screens/services/auth.dart
+++ lib/screens/services/auth.dart
@@ -1,14 +1,17 @@
+import 'package:brew_dude/models/Cuser.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService{
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  CUser? _userFromFirebaseUser(User? user){
+    return user==null?null  : CUser(uid : user.uid);
+  }
   Future signInAnon() async{
     try{
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user ;
+      return _userFromFirebaseUser(user) ;
 
     }
     catch(e)
